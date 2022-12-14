2f9558a1d6234f97b0b3a11edddf8186eac625c5
bright <32719579+bright2048@users.noreply.github.com>
2022-12-14 18:04:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ rm -f /usr/local/bin/vncc
 chmod +x mkinst vncc
 cp ./mkinst /usr/local/bin
 cp ./vncc /usr/local/bin
+cp ./recover_inst.sh /etc/init.d/recover_inst.sh
+chmod 755 /etc/init.d/recover_inst.sh
+update-rc.d recover_inst.sh defaults 90
